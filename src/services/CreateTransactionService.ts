361868e268f4da7d9da5f5c5e791b5f6774f968f
petkovic43 <tajumentino@gmail.com>
2020-07-23 12:24:25 -0300

    comentários das alterações

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,60 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+
+      if (value > total) {
+        throw new AppError('The amount is greater than the balance', 400);
+      }
+    }
+
+    const categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let categoryId = '';
+
+    if (categoryExists) {
+      categoryId = categoryExists.id;
+    } else {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      const { id } = await categoriesRepository.save(newCategory);
+      categoryId = id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
